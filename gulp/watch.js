fa591c2f7ea459ae03b0a58666bbce20e0d18518
Заур <zaurmag411@gmail.com>
2019-12-15 13:13:43 +0300

    Fix bug

--- gulp/watch.js
+++ gulp/watch.js
@@ -3,7 +3,7 @@ module.exports = function () {
     $.gulp.task('watch', function() {
         $.gulp.watch(path.path.watch.json, {usePolling: true}, $.gulp.series('pug:build', 'critical:build'));
         $.gulp.watch(path.path.watch.pug, {usePolling: true}, $.gulp.series('pug:build', 'critical:build'));
-        $.gulp.watch(path.path.watch.style, {usePolling: true}, $.gulp.series('style:build', 'critical:build'));
+        $.gulp.watch(path.path.watch.style, {usePolling: true}, $.gulp.series('style:build', 'pug:build', 'critical:build'));
         $.gulp.watch(path.path.src.js, {usePolling: true}, $.gulp.parallel('js:build'));
         $.gulp.watch(path.path.src.img, {usePolling: true}, $.gulp.parallel('img:build'));
         $.gulp.watch(path.path.src.imgComp, {usePolling: true}, $.gulp.parallel('img:build'));
